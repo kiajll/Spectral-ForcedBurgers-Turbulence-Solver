83413f02e3ec748fa0ca83dd55bda20bc99c7303
Kiarash Jalali <73154381+kiajll@users.noreply.github.com>
2024-11-03 09:21:19 -0500

    Update main_solver.m

--- main_solver.m
+++ main_solver.m
@@ -17,29 +17,35 @@ clear all, close all, clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 01.Define Domain Length
 Lx = 1;                       % Length of Domain Function
+
 % 02.Inject Initial velocity field
 u_x0 = readmatrix(initial_Velocity_Field.txt);
 N = Length(u_x0);             % Number of points in the spatial domain
 u = u_x0;
 u(1,N+1) = u(1,1);            % Periodic Domain condition
+
 % 03.Spatial Discretization
 dx = Lx/N; 
 x_min = -Lx/2;
 x_max = Lx/2;
 x = linspace(x_min, x_max, N + 1);
+
 % 04. Inject Forcing field evolved in time
 F_xt = readmatrix(spectral_forcing.txt);
 [Nt,Nx]=size(F_xt);
+
 % 05.Time Discretization
 tf = 1;                       % Final Time
 dt = round(tf/Nt-1);          % Time step
 t_min = 0;
 t_max = 1;                    % No periodicity in time
 t = linspace(t_min, t_max, Nt);
+
 % 06.Define Kinematic Viscosity
 a = 1;                        % Convective velocity
 nu = a/100;                   % Kinematic viscosity (1,10,100,1000,10000)..........................change it
-%% 07.Define wave number for the test function 
+
+% 07.Define wave number for the test function 
 m = -N/2:(N/2)-1;             % harmonic number for the test function
 kx_m = (m*2*pi/Lx);           % wave number for the test function
 kx_m = fftshift(kx_m);        % Re-order fft wavenumbers
